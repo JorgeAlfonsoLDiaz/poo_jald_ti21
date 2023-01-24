039e0a230cd77717cbd9e30d891dcb510efdf892
Jorge Alfonso Luqueño Díaz <122810362+JorgeAlfonsoLDiaz@users.noreply.github.com>
2023-01-24 19:58:30 +0000

    CREATE programa2.py

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 |--|--|--|
 |1.|programa0.py|Hola mundo en Python!|
 |2.|programa1.py|Comentarios multilínea, comentarios       de una sola línea, concatenación de cadenas y          cadenas de caracteres.|
+|3.|programa2.py|Concatenación utilizando str.format.|
---
+++ semana2/programa2.py
@@ -0,0 +1,9 @@
+"""
+    Programa2
+    Nombre: Jorge Alfonso Luqueño Díaz
+    Fecha: 24/01/2023
+    Descripción: El uso de print y str.format
+"""
+variable1 = "Hola"  # Variable tipo
+variable2 = 619  #Variable tipo
+print("{} {}".format(variable1,variable2))
