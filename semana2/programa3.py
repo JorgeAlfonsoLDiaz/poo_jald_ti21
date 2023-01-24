676d6df009b8235a4f399e7f1b9151e709fdeeff
Jorge Alfonso Luqueño Díaz <122810362+JorgeAlfonsoLDiaz@users.noreply.github.com>
2023-01-24 20:45:47 +0000

    MODIFY archivo programa3.py

--- semana2/programa3.py
+++ semana2/programa3.py
@@ -2,11 +2,15 @@
     Programa3
     Nombre: Jorge Alfonso Luqueño Díaz
     Fecha: 24/01/2023
-    Descripción: El uso de print y operaciones       aritméticas.
+    Descripción: El uso de print y operaciones aritméticas.
 """
 numero1 = 10  # Variable tipo int
 numero2 = 5  # Variable tipo int
-resultado = numero1 + numero2  # Variable tipo int, resultado de una suma
-print(numero1 + numero2)  # Imprime resultado: 15
 
-print ("{}+{}={}".format(numero1,numero2,resultado))  # Imprime la operación aritmética completa
+print(numero1 + numero2  # Imprime resultado: 15
+
+print("{}+{}={}".format(numero1,numero2,numero1+numero2))  # Imprime la suma de numero1 y numero2
+
+print("{}={}*{}".format(numero1*numero2,numero1,numero2))  # Imprime el producto de numero1 y numero2
+
+print("El resultado de dividir {} / {} es {}".format(numero1,numero2,numero1/numero2))  # Imprime el texto "El resultado de dividir 10 / 5 es 2.0" utilizando los valores de las variables numero1 y numero2
