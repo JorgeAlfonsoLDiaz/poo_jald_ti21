5a53d051fe20d57c1daecba08f417dd24d67965f
Jorge Alfonso Luqueño Díaz <122810362+JorgeAlfonsoLDiaz@users.noreply.github.com>
2023-02-04 06:04:31 +0000

    CREATE archivo reto_1.py

---
+++ semana3/reto_1.py
@@ -0,0 +1,9 @@
+"""
+    Reto_1
+    Nombre: Jorge Alfonso Luqueño Díaz
+    Fecha: 03/02/2023
+    Descripción: Código en Python que imprime un 'Hola mundo' e imprime 'Gracias por responder' si es que el usuario devuelve      una respuesta
+"""
+respuesta = input("Hola mundo! ")
+if respuesta == "Hola":
+  print("Gracias por responder!")
