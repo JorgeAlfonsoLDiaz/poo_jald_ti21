fd9830e3f355f83fea1dba6d0e5700b5f63a8953
Jorge Alfonso Luqueño Díaz <122810362+JorgeAlfonsoLDiaz@users.noreply.github.com>
2023-02-06 05:53:22 +0000

    CREATE archivo reto_2.py

---
+++ semana3/reto_2.py
@@ -0,0 +1,15 @@
+"""
+    Reto_2
+    Nombre: Jorge Alfonso Luqueño Díaz
+    Fecha: 05/02/2023
+    Descripción: Código en Python que verifica la respuesta del usuario para calcular el resultado de una rifa.
+"""
+numero = input("Elija un número del 1 al 10: > ")
+print("Gracias por participar en esta rifa!")
+if int(numero) in [4,2,9]:
+  print("¡Felicidades! ¡Usted es el ganador oficial del fabuloso premio de 5 pesos!")
+elif int(numero) <= 10:
+  print("¡Vaya! ¡Parece que hoy no fue su día de suerte, gracias por participar!")
+else:
+  print("Por favor, inténtelo otra vez con un número válido.")
+  
