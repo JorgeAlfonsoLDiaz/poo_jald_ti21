375c11ae1b59525855afea8a7845e0630b735958
Jorge Alfonso Luqueño Díaz <122810362+JorgeAlfonsoLDiaz@users.noreply.github.com>
2023-02-07 03:58:13 +0000

    MODIFY archivo reto_3.py
    Actualizado el sistema de compra de objetos.

--- semana3/reto_3.py
+++ semana3/reto_3.py
@@ -4,10 +4,10 @@
     Fecha: 06/02/2023
     Descripción: Código en Python que simula el funcionamiento de una tienda de objetos en Final Fantasy I.
 """
-respuesta = "si"
+activo = True
 dinero = 500
 
-while respuesta == "si":
+while activo == True:
   if dinero < 500:
     print("\n|1 -> Poción - 60 G \n|2 -> Antídoto - 75 G \n|3 -> Saco de dormir - 75 G \n| \n| Dinero: {} G \n \n| > Salir".format(dinero))
     compra = input("> ")
@@ -24,7 +24,7 @@ while respuesta == "si":
       confirmar = input("¿Estáis seguros de querer comprar Poción por 60 G? (si / no) \n>")
       if confirmar == "si":
         dinero -= 60
-        print("¡Gracias por comprar!")
+        print("\n¡Gracias por comprar!")
       
   elif compra == "2":
     if dinero < 75:
@@ -33,7 +33,7 @@ while respuesta == "si":
       confirmar = input("¿Estáis seguros de querer comprar Antídoto por 75 G? (si / no) \n>")
       if confirmar == "si":
         dinero -= 75
-        print("¡Gracias por comprar!")
+        print("\n¡Gracias por comprar!")
       
   elif compra == "3":
     if dinero < 75:
@@ -42,7 +42,7 @@ while respuesta == "si":
       confirmar = input("¿Estáis seguros de querer comprar Saco de dormir por 75 G? (si / no) \n>")
       if confirmar == "si":
         dinero -= 75
-        print("¡Gracias por comprar!")
+        print("\n¡Gracias por comprar!")
 
   elif compra == "salir" or "exit" or "cancelar":
     break
@@ -51,4 +51,4 @@ while respuesta == "si":
     print("¿Qué? Lo siento, no conozco ese producto.")
 
   if confirmar == "si":
-    respuesta = input("¿Algo más? (si / no) \n>")
+    print("¿Algo más?\n")
