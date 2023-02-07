dff9d9002fc3b3a04c9828775fd07054f2bf2494
Jorge Alfonso Luqueño Díaz <122810362+JorgeAlfonsoLDiaz@users.noreply.github.com>
2023-02-07 03:53:08 +0000

    CREATE archivo reto_3.py

---
+++ semana3/reto_3.py
@@ -0,0 +1,54 @@
+"""
+    Reto_3
+    Nombre: Jorge Alfonso Luqueño Díaz
+    Fecha: 06/02/2023
+    Descripción: Código en Python que simula el funcionamiento de una tienda de objetos en Final Fantasy I.
+"""
+respuesta = "si"
+dinero = 500
+
+while respuesta == "si":
+  if dinero < 500:
+    print("\n|1 -> Poción - 60 G \n|2 -> Antídoto - 75 G \n|3 -> Saco de dormir - 75 G \n| \n| Dinero: {} G \n \n| > Salir".format(dinero))
+    compra = input("> ")
+  else:
+    print("\nBienvenidos!")
+    print("¿Qué necesitáis?")
+    print("\n|1 -> Poción - 60 G \n|2 -> Antídoto - 75 G \n|3 -> Saco de dormir - 75 G \n| \n| Dinero: {} G \n \n| > Salir".format(dinero))
+    compra = input("> ")
+  
+  if compra == "1":
+    if dinero < 60:
+      print("Lo siento, pero no tienes el dinero suficiente.")
+    else:
+      confirmar = input("¿Estáis seguros de querer comprar Poción por 60 G? (si / no) \n>")
+      if confirmar == "si":
+        dinero -= 60
+        print("¡Gracias por comprar!")
+      
+  elif compra == "2":
+    if dinero < 75:
+      print("Lo siento, pero no tienes el dinero suficiente.")
+    else:
+      confirmar = input("¿Estáis seguros de querer comprar Antídoto por 75 G? (si / no) \n>")
+      if confirmar == "si":
+        dinero -= 75
+        print("¡Gracias por comprar!")
+      
+  elif compra == "3":
+    if dinero < 75:
+      print("Lo siento, pero no tienes el dinero suficiente.")
+    else:
+      confirmar = input("¿Estáis seguros de querer comprar Saco de dormir por 75 G? (si / no) \n>")
+      if confirmar == "si":
+        dinero -= 75
+        print("¡Gracias por comprar!")
+
+  elif compra == "salir" or "exit" or "cancelar":
+    break
+    
+  else:
+    print("¿Qué? Lo siento, no conozco ese producto.")
+
+  if confirmar == "si":
+    respuesta = input("¿Algo más? (si / no) \n>")
