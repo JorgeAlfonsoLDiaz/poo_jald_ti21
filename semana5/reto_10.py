42e05bcf16df68ed25230a931e69349e83bddf00
Jorge Alfonso Luqueño Díaz <122810362+JorgeAlfonsoLDiaz@users.noreply.github.com>
2023-02-15 05:58:13 +0000

    CREATE archivo reto_10.py

---
+++ semana5/reto_10.py
@@ -0,0 +1,14 @@
+"""
+    Reto_9"
+    Nombre: Jorge Alfonso Luqueño Díaz
+    Fecha: 14/02/2023
+    Descripción: Código que pide una entrada de tres números enteros, e imprime en pantalla el resultado de su suma.
+"""
+
+numero1 = input("Introduzca el primer número: ")  # Guarda en la variable "numero1" el valor de la entrada de datos del usuario.
+numero2 = input("Introduzca el segundo número: ")  # Guarda en la variable "numero2" el valor de la entrada de datos del usuario.
+numero3 = input("Introduzca el tercer número: ")  # Guarda en la variable "numero3" el valor de la entrada de datos del usuario.
+
+resultado = int(numero1) + int(numero2) + int(numero3)  # Guarda en la variable "resultado" la suma de numero1, numero2 y numero3.
+
+print(resultado)  # Imprime en pantalla el valor de la variable "resultado".
