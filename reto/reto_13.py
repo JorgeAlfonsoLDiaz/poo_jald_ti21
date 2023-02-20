b8395ee47fe6a8bf8111b4f564d63210ebc2c584
Jorge Alfonso Luqueño Díaz <122810362+JorgeAlfonsoLDiaz@users.noreply.github.com>
2023-02-20 05:36:17 +0000

    CREATE archivo reto_13.py

---
+++ reto/reto_13.py
@@ -0,0 +1,11 @@
+"""
+    Reto_13"
+    Nombre: Jorge Alfonso Luqueño Díaz
+    Fecha: 19/02/2023
+    Descripción: Código que pide la respuesta del usuario para entregarle un éxito.
+"""
+
+respuesta = input("Hola, ¿Quieres tener éxito? (y/n) > ")  # Le pide una respuesta al usuario, y la guarda en la variable "respuesta"
+
+if respuesta == "y":  # Evalúa si la respuesta corresponde al caracter especificado, con una sentencia if
+  print("|Éxito| x1")  # Imprime en pantalla un mensaje "|Éxito| x1".
