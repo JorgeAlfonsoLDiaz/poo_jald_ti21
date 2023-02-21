0de53d551b5233c6a4c5b7758993cbd00a6fc44a
Jorge Alfonso Luqueño Díaz <122810362+JorgeAlfonsoLDiaz@users.noreply.github.com>
2023-02-21 05:58:47 +0000

    CREATE archivo reto_14.py

---
+++ reto/reto_14.py
@@ -0,0 +1,11 @@
+"""
+    Reto_14"
+    Nombre: Jorge Alfonso Luqueño Díaz
+    Fecha: 20/02/2023
+    Descripción: Código que saluda al usuario, y también al revés.
+"""
+
+saludo = input("Hola! ")
+
+if saludo == "Hola mundo":
+  print("Espera, ¿no era al revés? Bueno, da igual, ¡hola!")
